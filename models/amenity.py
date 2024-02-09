826b63ffd8cc626079488d63d719deb05a0526ea
Yoftahe Gizaw <140615264+Yofgiz@users.noreply.github.com>
2024-02-09 10:27:17 +0300

    Create amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defining the amenity class"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+        name (str): The name of the amenity
+    """
+    name = ""
